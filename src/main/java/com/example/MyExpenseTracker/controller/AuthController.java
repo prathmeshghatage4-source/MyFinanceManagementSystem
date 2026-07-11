dd6a531cad989ba2e59c8d3e741db41d08a3b0ec
Prathmesh-Ghatage <prathmesh.ghatage20@pccoepune.org>
2026-07-11 15:07:27 +0530

    Move registration to /auth/register, fix security config

--- src/main/java/com/example/MyExpenseTracker/controller/AuthController.java
+++ src/main/java/com/example/MyExpenseTracker/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.example.MyExpenseTracker.controller;
 
 import com.example.MyExpenseTracker.dto.LoginRequestDTO;
+import com.example.MyExpenseTracker.dto.UserRequestDTO;
+import com.example.MyExpenseTracker.dto.UserResponseDTO;
+import com.example.MyExpenseTracker.entity.User;
 import com.example.MyExpenseTracker.service.AuthService;
 import com.example.MyExpenseTracker.service.JWTService;
+import com.example.MyExpenseTracker.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
-
+    private final UserService userService;
 
     @PostMapping("/login")
     public String verifyCredential(@RequestBody LoginRequestDTO dto){
         return authService.verifyCredential(dto);
     }
 
+    @PostMapping("/register")
+    public UserResponseDTO addUser(@Valid @RequestBody UserRequestDTO dto){
+        return userService.addUser(dto);
+    }
 }
